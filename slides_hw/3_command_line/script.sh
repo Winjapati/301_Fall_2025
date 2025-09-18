1e44b440d50da4eefcf9702a92c6680883c61c48
Winjapati <andrewbyrd@uky.edu>
2025-09-18 14:20:32 -0400

    day 13 done

--- slides_hw/3_command_line/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash                 
-echo "This is my first script."
-curl -o term_comm.txt https://www.gutenberg.org/files/1342/1342-0.txt
-mkdir folder_1
-mv term_comm.txt folder_1
-echo term_comm.txt
