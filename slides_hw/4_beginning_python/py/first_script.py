7133b961c2d1b011be5874f7f1657a3f6f5ad218
Winjapati <andrewbyrd@uky.edu>
2025-09-30 15:30:56 -0400

    20

--- slides_hw/4_beginning_python/py/first_script.py
+++ slides_hw/4_beginning_python/py/first_script.py
@@ -1,1 +1,6 @@
-print("Hello world!")
+import re
+
+text = "Hello, world!   This... is a test."
+words = re.split(r"[\s\W]+", text)
+print(words)
+# ['Hello', 'world', 'This', 'is', 'a', 'test', '']
