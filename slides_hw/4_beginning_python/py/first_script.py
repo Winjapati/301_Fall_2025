10048079042e2224eac6a16dd6f4b46685f808b5
Winjapati <andrewbyrd@uky.edu>
2025-10-03 17:56:34 -0400

    18 updated

--- slides_hw/4_beginning_python/py/first_script.py
+++ slides_hw/4_beginning_python/py/first_script.py
@@ -1,6 +1,20 @@
-import re
+novel = "Wuthering Heights" 
 
-text = "Hello, world!   This... is a test."
-words = re.split(r"[\s\W]+", text)
-print(words)
-# ['Hello', 'world', 'This', 'is', 'a', 'test', '']
+charlotte = ["The Professor", "Jane Eyre", "Shirley", "Villette"] 
+emily = ["Wuthering Heights"] 
+anne = ["Agnes Grey", "The Tenant of Wildfell Hall"] 
+
+bronte_checks = {"charlotte": 0, "emily": 0, "anne" : 0} 
+
+if novel in charlotte: 
+    bronte_checks["charlotte"] = bronte_checks["charlotte"] + 1
+    print("Charlotte Brontë wrote", novel) 
+elif novel in emily: 
+    bronte_checks["emily"] = bronte_checks["emily"] + 1
+    print("Emily Brontë wrote", novel)
+elif novel in anne: 
+    bronte_checks["anne"] = bronte_checks["anne"] + 1
+    print("Anne Brontë wrote", novel) 
+else: print(novel, "was not written by one of the Brontë sisters")
+
+print(bronte_checks)
