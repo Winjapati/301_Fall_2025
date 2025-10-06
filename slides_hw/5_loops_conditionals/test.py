ba64a2f39457cded22d1ba4870306344fa842ba8
Andrew Byrd <andrewbyrd@uky.edu>
2025-10-06 12:50:31 -0400

    18 in class

---
+++ slides_hw/5_loops_conditionals/test.py
@@ -0,0 +1,15 @@
+andrew = 46
+alex = 12
+bryce = 22
+elijah = 8
+brenna = 47
+joey = 18
+kendal = 20
+
+names = [andrew, alex, bryce, elijah, brenna, joey, kendal]
+
+for name in names:
+    if name % 2 == 0:
+        print(name, "is even years old.")
+    else:
+        print(name, "is odd years old.")
